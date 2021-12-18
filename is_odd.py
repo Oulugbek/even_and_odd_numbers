e1f3c2c6730a1d8d9490f12ddfe4781b7cdaba16
Ulugbek <61067364+Oulugbek@users.noreply.github.com>
2021-12-18 20:29:19 +0500

    Update is_odd.py

--- is_odd.py
+++ is_odd.py
@@ -1,6 +1,5 @@
 #Create a variable "number" and assign it a integer value.
-
 #Print 1 if the number is odd, otherwise print 0.
-a = input()
-int(a)
-print(a%2)
+number = input()
+int(number)
+print(number%2)
