16ded103dc9b97672b4a7c816eb3b7dc877b28be
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-19 11:42:15 +0500

    2

--- is_odd.py
+++ is_odd.py
@@ -1,4 +1,5 @@
 #Create a variable "number" and assign it a integer value.
 #Print 1 if the number is odd, otherwise print 0.
-var_int = 4568
-print(4-var_int//1000%2-var_int//100%2-var_int//10%2-var_int%2)
+number = 45
+int(number)
+print(number%2)
