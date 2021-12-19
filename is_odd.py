71bb8757fa9e5c2099432c3d1280bbda7ea731ba
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-19 11:07:18 +0500

    number_even

--- is_odd.py
+++ is_odd.py
@@ -1,5 +1,4 @@
 #Create a variable "number" and assign it a integer value.
 #Print 1 if the number is odd, otherwise print 0.
-number = 45
-int(number)
-print(number%2)
+var_int = 4568
+print(4-var_int//1000%2-var_int//100%2-var_int//10%2-var_int%2)
